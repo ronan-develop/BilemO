6ee2387b944ceb83819cb2e29d38438162732245
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-11-01 15:18:00 +0100

    🐞 Fixes: HttpException

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -2,10 +2,10 @@
 
 namespace App\EventSubscriber;
 
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class ExceptionSubscriber implements EventSubscriberInterface
@@ -13,19 +13,17 @@ class ExceptionSubscriber implements EventSubscriberInterface
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
-
         if ($exception instanceof HttpException) {
             $data = [
                 'status' => $exception->getStatusCode(),
                 'message' => $exception->getMessage()
             ];
-
         } else {
+
             $data = [
                 'status' => 500,
                 'message' => $exception->getMessage()
             ];
-
         }
         $event->setResponse(new JsonResponse($data));
     }
