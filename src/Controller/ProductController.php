638cb7046c7cf1e65ce01018d9ced69185255f23
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-11-02 17:44:00 +0100

    📋 docs: each route is documented

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,8 @@ use App\Entity\Product;
 use App\Services\ProductService;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -22,9 +24,19 @@ class ProductController extends AbstractController
     }
 
     /**
+     * Method to get all Products
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @return JsonResponse
      * @throws InvalidArgumentException
      */
     #[Route('/api/products', name: 'app_products', methods: ['GET'])]
+    #[OA\Response(
+        response: 200, description: "Return all products in the catalogue", content: new OA\JsonContent(
+        type: 'array',
+        items: new OA\Items(ref: new Model(type: AlbumDto::class, groups: ['full']))
+    )
+    )]
     public function getAllProducts(SerializerInterface $serializer, Request $request): JsonResponse
     {
         $context = SerializationContext::create()->setGroups(['getProducts']);
@@ -35,6 +47,13 @@ class ProductController extends AbstractController
         return new JsonResponse($json, Response::HTTP_OK, [], true);
     }
 
+    /**
+     * Method to access product details
+     * @param Product $product
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @return JsonResponse
+     */
     #[Route('/api/products/{id}', name: 'app_product_details', methods: ['GET'])]
     public function getOneProduct(Product $product, SerializerInterface $serializer, Request $request): JsonResponse
     {
